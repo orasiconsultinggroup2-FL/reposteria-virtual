0f4e12da6334475a3f2e3b712534bea9de52bfd5
orasiconsultinggroup2-FL <orasi.consulting.group2@gmail.com>
2026-03-28 15:24:08 -0500

    Create Header.tsx

---
+++ src/src/src/src/src/src/components/Layout/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-[#F7EDE2] text-[#2C1E16] p-4 shadow-md">
+      <h1 className="text-xl font-bold">Repostería Virtual</h1>
+    </header>
+  );
+}
